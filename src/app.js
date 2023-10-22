fe452d396b42dccf03af3b4a09f3941fd3994929
OUHernandezAyala <142889499+OUHernandezAyala@users.noreply.github.com>
2023-10-22 05:08:06 +0000

    Uploading my project Cards Random with the HTML, CSS, and JS files.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let cardNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  document.querySelectorAll("p.cardSuit").forEach(function(element) {
+    element.innerHTML = randomSuit;
+    if (randomSuit === "♦" || randomSuit === "♥") {
+      element.style.color = "red";
+    }
+  });
+  document.querySelector("#card-number").innerHTML =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
 };
